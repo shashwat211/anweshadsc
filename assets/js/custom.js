3ec1e961403803d2d9863ed3c0fe7f9956da4eee
Shashwat Mahajan <shashwat211@gmail.com>
2020-01-04 15:29:54 +0530

    Added Maps, Venue and mailing details

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -42,7 +42,7 @@
 	/*  2. EVENT TIME COUNTER
 	/* ----------------------------------------------------------- */
 	
-	$('#mu-event-counter').countdown('2020/02/10').on('update.countdown', function(event) {
+	$('#mu-event-counter').countdown('2020/01/31').on('update.countdown', function(event) {
 	  var $this = $(this).html(event.strftime(''
 	    + '<span class="mu-event-counter-block"><span>%D</span> Days</span> '
 	    + '<span class="mu-event-counter-block"><span>%H</span> Hours</span> '
--- mailer.php
+++ mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@markups.io";
+        $recipient = "shashwat211@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
